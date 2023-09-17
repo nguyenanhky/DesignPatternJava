2bc54310fd6b298e75a10d33458380892529a927
nguyenanhky <chienky2016@gmail.com>
2023-09-17 09:16:53 +0700

    Singleton

--- src/ClientTest.java
+++ src/ClientTest.java
@@ -2,8 +2,11 @@ public class ClientTest {
     public static void main(String[] args) {
         EagerInitialization eagerInitialization = EagerInitialization.getInstance();
         eagerInitialization.setName("nguyen anh ky");
+        System.out.println(eagerInitialization);
 
         EagerInitialization eagerInitialization1= EagerInitialization.getInstance();
         System.out.println(eagerInitialization1.getName());
+        System.out.println(eagerInitialization1);
+
     }
 }
---
+++ src/ThreadSafe.java
@@ -0,0 +1,15 @@
+public class ThreadSafe {
+    private static volatile ThreadSafe INSTANCE;
+
+    private ThreadSafe() {
+
+    }
+
+    public static synchronized ThreadSafe getInstance() {
+        if(INSTANCE == null) {
+            INSTANCE = new ThreadSafe();
+        }
+        return INSTANCE;
+    }
+}
+
